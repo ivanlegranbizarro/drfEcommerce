31e5c780b6ff92da98a488e8fce8f9656544395f
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-04-14 10:58:23 +0200

    implementing brand

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet
+from .views import BrandViewSet, CategoryViewSet
 
 router = DefaultRouter()
 
 router.register(r"categories", CategoryViewSet, basename="category")
+router.register(r"brands", BrandViewSet, basename="brand")
