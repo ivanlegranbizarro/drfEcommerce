f026aa213931e99b2b171efdf98b717980e1de9e
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-04-14 11:19:25 +0200

    improving serializers

--- product/serializers.py
+++ product/serializers.py
@@ -1,23 +1,24 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import Brand, Category, Product
 
 
-class CategorySerializer(ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
 
 
-class BrandSerializer(ModelSerializer):
+class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = "__all__"
 
 
-class ProductSerializer(ModelSerializer):
-    brand = BrandSerializer()
-    category = CategorySerializer()
+class ProductSerializer(serializers.ModelSerializer):
+    brand_name = serializers.CharField(source='brand.name', read_only=True)
+    category_name = serializers.CharField(
+        source='category.name', read_only=True)
 
     class Meta:
         model = Product
