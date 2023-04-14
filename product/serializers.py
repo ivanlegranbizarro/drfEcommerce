75f4d89009a4eccf946638d13ca1387035b5c60c
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-04-14 08:55:24 +0200

    serializers and CategoryViewSet

---
+++ product/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Brand, Category, Product
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class BrandSerializer(ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = "__all__"
+
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
