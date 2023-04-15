cc6a888152811fbeca84359b92feac032e7e0668
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-04-15 11:14:04 +0200

    endpoint tests

---
+++ tests/product/test_endpoints.py
@@ -0,0 +1,38 @@
+import pytest
+from django.urls import reverse
+
+pytestmark = pytest.mark.django_db
+
+
+class TestCategoryEndpoints:
+    url = reverse("category-list")
+
+    def test_category_get(self, category_factory, api_client):
+        category = category_factory()
+        response = api_client.get(self.url)
+        assert response.status_code == 200
+        assert response.data[0]["name"] == str(category.name)
+
+
+class TestBrandEndpoints:
+    url = reverse("brand-list")
+
+    def test_brand_get(self, brand_factory, api_client):
+        brand = brand_factory()
+        response = api_client.get(self.url)
+        assert response.status_code == 200
+        assert response.data[0]["name"] == str(brand.name)
+
+
+class TestProductEndpoints:
+    url = reverse("product-list")
+
+    def test_product_get(self, product_factory, api_client):
+        product = product_factory()
+        response = api_client.get(self.url)
+        assert response.status_code == 200
+        assert response.data[0]["name"] == str(product.name)
+        assert response.data[0]["brand"] == product.brand.id
+        assert response.data[0]["category"] == product.category.id
+        assert response.data[0]["description"] == str(product.description)
+        assert response.data[0]["is_digital"] == product.is_digital
