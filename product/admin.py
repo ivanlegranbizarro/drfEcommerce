c82aebdfb23316eb7927eee57dc8fe8b25440b7f
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-04-16 08:57:58 +0200

    product line implemented

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Product, Brand
+from .models import Category, Product, Brand, ProductLine
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Brand)
+admin.site.register(ProductLine)
--- product/serializers.py
+++ product/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Brand, Category, Product
+from .models import Brand, Category, Product, ProductLine
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -22,4 +22,12 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
+        fields = "__all__"
+
+
+class ProductLineSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name', read_only=True)
+
+    class Meta:
+        model = ProductLine
         fields = "__all__"
